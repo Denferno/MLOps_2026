fix correct torch.no_grad, validation now works and disables gradient computation.

src/ml_core/solver/trainer.py
@@ -94,27 +94,40 @@ class Trainer:
         
         # TODO: Implement Validation Loop
         # Remember: No gradients needed here
-        self.val_loss = 0.0
-        self.val_correct = 0
-        self.val_total = 0
 
-        with torch.no_grad:
+        #Het verschil is dat validation geen learning doet en alleen testen. Ook heeft het geen gradient berekening. En zit het in eval mode 
+        running_loss = 0.0
+        total_correct = 0
+        total_samples = 0
+        all_pred = []
+        all_labels = []
+
+        with torch.no_grad():
             for image, label in dataloader:
                 image = image.to(self.device)
                 label = label.to(self.device)
 
+                # dit is de forward
                 output = self.model(image)
+
+                # compute loss
                 loss = self.criterion(output, label)
+                
+                # hier zit dus geen backpropagation (in train_ep wel, dus gaat het niet leren
 
-                self.val_loss = loss.item()
+                batch_size = label.size(0)
+                running_loss += loss.item() * batch_size
+                total_samples += batch_size
 
-                _, pred = torch.max(output.data, 1)
-                total_pred += label.size(0)
-                correct_pred += (pred == label).sum().items()
+                preds = torch.argmax(output, dim=1)
+                total_correct += (preds == label).sum().item()
 
-        avg_loss = self.val_loss / len(dataloader)
-        accuracy = correct_pred / total_pred
-        f1 = f1_score(label.data,total_pred)
+                all_pred.extend(preds.cpu().numpy())
+                all_labels.extend(label.cpu().numpy())
+
+        avg_loss = running_loss / total_samples
+        accuracy = total_correct / total_samples
+        f1 = f1_score(all_labels, all_pred, average='binary')
 
         return avg_loss, accuracy, f1
 